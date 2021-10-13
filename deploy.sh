7b817de1d983ec73d8934c546c9962ed7a361cc0
rio <Roman.Ionenko@partners.tecalliance.net>
2021-10-13 14:23:02 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t bgahavan/multi-client:latest -t bgahavan/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t bgahavan/multi-server:latest -t bgahavan/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t bgahavan/multi-worker:latest -t bgahavan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
+docker push bgahavan/multi-client:latest
+docker push bgahavan/multi-server:latest
+docker push bgahavan/multi-worker:latest
 
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push bgahavan/multi-client:$SHA
+docker push bgahavan/multi-server:$SHA
+docker push bgahavan/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=bgahavan/multi-server:$SHA
+kubectl set image deployments/client-deployment client=bgahavan/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=bgahavan/multi-worker:$SHA
